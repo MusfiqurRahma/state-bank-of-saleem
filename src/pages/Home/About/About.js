d46399d4768f8f7d7f50dabee15cd9d612468d02
Musfiqur Rahma <musfiq371@gmail.com>
2022-01-24 17:01:31 +0600

    added-about

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -1,27 +1,60 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import firstImg from '../../../image/gettyimages-1137620220-170667a.jpg';
-import secondImg from '../../../image/Top-15-Best-Laptop-Backpacks-for-Women-In-2020.jpg';
+import secondImg from '../../../image/image.png';
+import firstImg from '../../../image/laptop women.jpeg';
 
 const About = () => {
     return (
         <Container>
              <Row style={{marginTop:'250px'}}>
                 <Col sx={12} md={6}>
-                    <div style={{marginLeft:'150px'}}>
-                        <p style={{ backgroundColor: '#1521cf', width: '100px', fontSize: '35px',color:'white',fontFamily:'poppins'}}>100 <span style={{fontSize:'16px'}}><br/>Awards</span></p>
+                    <div style={{}}>
+                        <p
+                            style={{
+                                marginBottom:'-12%',
+                                marginLeft:'100px',
+                                backgroundColor: '#1521cf',
+                                width: '205px',
+                                height:'104px',
+                                fontSize: '35px',
+                                color: 'white',
+                                fontFamily: 'poppins',
+                            }}>100 <span style={{ fontSize: '16px'}}><br />Awards</span></p>
                     </div>
                     <div>
-                        <img style={{marginLeft:'250px',marginTop:'-12%'}} src={firstImg} alt="" />
+                        <img
+                            style={{
+                                marginRight: '500px', marginTop: '20%',
+                                width: '432px',
+                                height: '498px'
+                            }} src={firstImg} alt="" />
                     </div>
                     <div>
-                    <img style={{marginRight:'120px',marginTop:'-50%'}} src={secondImg} alt="" />
+                        <img
+                            style={{
+                                marginLeft: '330px', marginTop: '-150%',
+                                width: '432px',
+                                height: '498px'
+                            }} src={secondImg} alt="" />
                     </div>
                     <div>
-                    <p style={{ backgroundColor: '#1521cf', width: '100px', fontSize: '35px',marginLeft:'90%',color:'white',fontFamily:'poppins'}}>3.5m <span style={{fontSize:'16px'}}><br/>Accounts</span></p>
+                        <p
+                            style={{
+                                marginTop:'-23%',
+                                marginLeft:'84%',
+                                backgroundColor: '#1521cf',
+                                width: '205px',
+                                height:'104px',
+                                fontSize: '35px',
+                                color: 'white',
+                                fontFamily: 'poppins',
+                            }}>3.5m <span style={{ fontSize: '16px' }}><br />Accounts</span></p>
                     </div>
               </Col>
-              <Col sx={12} md={6}>2 of 2</Col>
+                <Col sx={12} md={6}>
+                    <p>About us</p>
+                    <p>Not Fast Decisions But Good <span style={{color:'#1521cf'}}>Decisions</span> Fast</p>
+              </Col>
             </Row>
         </Container>
     );
